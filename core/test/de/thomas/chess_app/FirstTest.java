62a836499cfc3b9aeeed1ec8a6e36e39da069523
Thomas Opitz <opitz-t@web.de>
2015-10-22 17:38:57 +0200

    Added first test

---
+++ core/test/de/thomas/chess_app/FirstTest.java
@@ -0,0 +1,11 @@
+package de.thomas.chess_app;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class FirstTest {
+    @Test
+    public void thisAlwaysPasses() {
+        assertTrue(true);
+    }
+}
