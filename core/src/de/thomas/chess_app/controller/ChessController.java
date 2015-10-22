0159938a1d4f244892f3b1d4256cd70e8059c3ba
Thomas Opitz <opitz-t@web.de>
2015-10-22 16:54:01 +0200

    Refactoring of tryMove method

--- core/src/de/thomas/chess_app/controller/ChessController.java
+++ core/src/de/thomas/chess_app/controller/ChessController.java
@@ -44,37 +44,14 @@ public class ChessController {
             return false;
         }
 
-
         Position position = chessGame.getPosition();
 
         int stone = position.getStone(startSqi);
 
         if (stone != 0) {
-            short move;
+            short move = getMove(stone, startSqi, endSqi);
 
-            //Castling
-            if (stone == Chess.WHITE_KING && startSqi == Chess.E1 && endSqi == Chess.G1) {
-                move = Move.getShortCastle(Chess.WHITE);
-            }
-            else if (stone == Chess.WHITE_KING && startSqi == Chess.E1 && endSqi == Chess.C1) {
-                move = Move.getLongCastle(Chess.WHITE);
-            }
-            else if (stone == Chess.BLACK_KING && startSqi == Chess.E8 && endSqi == Chess.G8) {
-                move = Move.getShortCastle(Chess.BLACK);
-            }
-            else if (stone == Chess.BLACK_KING && startSqi == Chess.E8 && endSqi == Chess.C8) {
-                move = Move.getLongCastle(Chess.BLACK);
-            }
-            //En passant
-            else if (position.getPiece(startSqi) == Chess.PAWN && position.getPiece(endSqi) == 0 && startSqi % 2 != endSqi % 2) {
-                move = Move.getEPMove(startSqi, endSqi);
-            }
-            else {
-                boolean capturing = position.getPiece(endSqi) != 0;
-                move = Move.getRegularMove(startSqi, endSqi, capturing);
-            }
-
-            short[] possibleMoves = chessGame.getPosition().getAllMoves();
+            short[] possibleMoves = position.getAllMoves();
 
             for (short possibleMove : possibleMoves) {
                 if (move == possibleMove) {
@@ -90,5 +67,35 @@ public class ChessController {
         }
 
         return false;
+    }
+
+    private short getMove(int stone, int startSqi, int endSqi) {
+        Position position = chessGame.getPosition();
+
+        short move;
+
+        //Castling
+        if (stone == Chess.WHITE_KING && startSqi == Chess.E1 && endSqi == Chess.G1) {
+            move = Move.getShortCastle(Chess.WHITE);
+        }
+        else if (stone == Chess.WHITE_KING && startSqi == Chess.E1 && endSqi == Chess.C1) {
+            move = Move.getLongCastle(Chess.WHITE);
+        }
+        else if (stone == Chess.BLACK_KING && startSqi == Chess.E8 && endSqi == Chess.G8) {
+            move = Move.getShortCastle(Chess.BLACK);
+        }
+        else if (stone == Chess.BLACK_KING && startSqi == Chess.E8 && endSqi == Chess.C8) {
+            move = Move.getLongCastle(Chess.BLACK);
+        }
+        //En passant
+        else if (position.getPiece(startSqi) == Chess.PAWN && position.getPiece(endSqi) == 0 && startSqi % 2 != endSqi % 2) {
+            move = Move.getEPMove(startSqi, endSqi);
+        }
+        else {
+            boolean capturing = position.getPiece(endSqi) != 0;
+            move = Move.getRegularMove(startSqi, endSqi, capturing);
+        }
+
+        return move;
     }
 }
